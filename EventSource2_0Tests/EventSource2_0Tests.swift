79939e6f805b666ab0e173fcffaed8abf0c42ac2
shashankprabhakar <shashank.dwayne@gmail.com>
2020-04-26 17:29:36 +0100

    Initial Commit

---
+++ EventSource2_0Tests/EventSource2_0Tests.swift
@@ -0,0 +1,34 @@
+//
+//  EventSource2_0Tests.swift
+//  EventSource2_0Tests
+//
+//  Created by Shashank Prabhakar on 26/04/2020.
+//  Copyright © 2020 Shashank Prabhakar. All rights reserved.
+//
+
+import XCTest
+@testable import EventSource2_0
+
+class EventSource2_0Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

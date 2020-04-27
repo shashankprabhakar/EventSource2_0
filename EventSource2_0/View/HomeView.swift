844fa6388988ffb9b60b34ddbac7c9809add7380
shashankprabhakar <shashank.dwayne@gmail.com>
2020-04-27 18:27:03 +0100

    add list to scroll view

--- EventSource2_0/View/HomeView.swift
+++ EventSource2_0/View/HomeView.swift
@@ -10,18 +10,64 @@ import SwiftUI
 
 struct HomeView: View {
     @State private var val: String = ""
+    var listOfEvent = [
+    "Movies",
+    "Music",
+    "Sports",
+    "Food"
+    ]
     
     var body: some View {
         VStack {
             TextField("placeholder", text: $val)
-                .background(<#T##background: View##View#>)
+            .padding()
+            .background(Color.red)
+            
+            ScrollView(.horizontal) {
+                
+                HStack {
+                    
+                    ForEach(0 ..< listOfEvent.count) { i in
+                    
+                        Text("\(self.listOfEvent[i])")
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(15)
+                        .font(.system(size: 15, weight: .bold))
+                
+           
+                  
+                }
+            }
+                
+        }
+                
+                
+            .padding()
+            .background(Color.green)
+            
+            
+            VStack{
+                Text("This is the VStack")
+                Text("This is the VStack")
+                Text("This is the VStack")
+                Text("This is the VStack")
+                
+            }
+            
+            .padding()
+            .background(Color.blue)
+            
             Text("This is home view")
             Spacer()
-            
-        }
-    }
-}
 
+            }
+        
+        }
+        
+        
+    
+}
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
